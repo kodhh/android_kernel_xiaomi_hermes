198763214540c06606b6dc100dce6de07787aa9b
root <root@DESKTOP-7MIFRS6.localdomain>
2026-04-16 17:04:06 +0800

    vdso32

--- arch/arm64/include/asm/elf.h
+++ arch/arm64/include/asm/elf.h
@@ -167,7 +167,11 @@ typedef compat_elf_greg_t		compat_elf_gregset_t[COMPAT_ELF_NGREG];
 
 #define compat_start_thread		compat_start_thread
 #define COMPAT_SET_PERSONALITY(ex)	set_thread_flag(TIF_32BIT);
+#ifdef CONFIG_VDSO32
+#define COMPAT_ARCH_DLINFO		_SET_AUX_ENT_VDSO
+#else
 #define COMPAT_ARCH_DLINFO
+#endif
 extern int aarch32_setup_vectors_page(struct linux_binprm *bprm,
 				      int uses_interp);
 #define compat_arch_setup_additional_pages \
