8cce8027b3b6f4e8f36517e7a4a500c11dbfd759
kodhh <89465249+kodhh@users.noreply.github.com>
2026-05-17 09:29:39 +0800

    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -301,8 +301,9 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 }
 
 #ifdef CONFIG_KSU
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-			                    int *flags);
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
 #endif
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
