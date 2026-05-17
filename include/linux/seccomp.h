4480620fbd483e123f079121ecbab3092529b077
root <root@runnervmrw5os.sriysyaqsrbelmpkiefzon32ue.phxx.internal.cloudapp.net>
2026-05-17 01:48:06 +0000

    revert Subject: [PATCH] KernelSU: add prctl manual hook, seccomp filter_count, update

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -8,7 +8,6 @@
 #ifdef CONFIG_SECCOMP
 
 #include <linux/thread_info.h>
-#include <linux/atomic.h>
 #include <asm/seccomp.h>
 
 struct seccomp_filter;
@@ -25,7 +24,6 @@ struct seccomp_filter;
  */
 struct seccomp {
 	int mode;
-	atomic_t filter_count;
 	struct seccomp_filter *filter;
 };
 
