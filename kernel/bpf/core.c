6bf9fe6681dc2fc3ed050819293fca8bf52c222d
root <root@runnervm1li68.djo20zqc0iyezjaecdggufo4df.bx.internal.cloudapp.net>
2026-06-16 09:30:16 +0000

    bpf: backport tail call opcode, stack tracking, and exec alloc fixes
    - BPF_TAIL_CALL: new opcode 0xf0 replaces BPF_JMP|BPF_CALL|BPF_X
      (include/linux/filter.h, kernel/bpf/core.c, kernel/bpf/verifier.c,
       arch/arm64/net/bpf_jit_comp.c)
    - verifier tracks program stack depth (include/linux/bpf.h,
      kernel/bpf/verifier.c)
    - Add __weak bpf_jit_alloc_exec / bpf_jit_free_exec hooks for executable
      memory allocation, with CONFIG_MODULES fallback to vmalloc_exec
      (kernel/bpf/core.c)
    - Allow BPF_JIT without MODULES support (net/Kconfig)

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -309,6 +309,24 @@ bool __weak arch_bpf_jit_check_func(const struct bpf_prog *prog)
 EXPORT_SYMBOL(arch_bpf_jit_check_func);
 #endif
 
+void *__weak bpf_jit_alloc_exec(unsigned long size)
+{
+#ifdef CONFIG_MODULES
+	return module_alloc(size);
+#else
+	return vmalloc_exec(size);
+#endif
+}
+
+void __weak bpf_jit_free_exec(void *addr)
+{
+#ifdef CONFIG_MODULES
+	module_free(NULL, addr);
+#else
+	vfree(addr);
+#endif
+}
+
 struct bpf_binary_header *
 bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 		     unsigned int alignment,
@@ -326,7 +344,7 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 
 	if (bpf_jit_charge_modmem(pages))
 		return NULL;
-	hdr = module_alloc(size);
+	hdr = bpf_jit_alloc_exec(size);
 	if (!hdr) {
 		bpf_jit_uncharge_modmem(pages);
 		return NULL;
@@ -351,7 +369,7 @@ void bpf_jit_binary_free(struct bpf_binary_header *hdr)
 {
 	u32 pages = hdr->pages;
 
-	module_free(NULL, hdr);
+	bpf_jit_free_exec(hdr);
 	bpf_jit_uncharge_modmem(pages);
 }
 
@@ -635,7 +653,7 @@ static unsigned int __bpf_prog_run(const struct sk_buff *ctx, const struct bpf_i
 		[BPF_ALU64 | BPF_NEG] = &&ALU64_NEG,
 		/* Call instruction */
 		[BPF_JMP | BPF_CALL] = &&JMP_CALL,
-		[BPF_JMP | BPF_CALL | BPF_X] = &&JMP_TAIL_CALL,
+		[BPF_JMP | BPF_TAIL_CALL] = &&JMP_TAIL_CALL,
 		/* Jumps */
 		[BPF_JMP | BPF_JA] = &&JMP_JA,
 		[BPF_JMP | BPF_JEQ | BPF_X] = &&JMP_JEQ_X,
