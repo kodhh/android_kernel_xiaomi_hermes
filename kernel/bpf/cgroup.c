22bef52c0b2d3ce475a4bdf630b10dd547612a4b
root <root@runnervmkkn4f.0bstqgub2uperkwdrrb1qd0e0g.cx.internal.cloudapp.net>
2026-07-03 02:12:42 +0000

    bpf: fix double computation/activation of effective prog arrays in attach/detach
    __cgroup_bpf_attach and __cgroup_bpf_detach computed and activated
    effective prog arrays for cgrp itself explicitly, then again via
    css_for_each_descendant_pre (which starts from cgrp). The activation
    phase was particularly harmful: the explicit call set cgrp->bpf.inactive
    to NULL, then the loop called activate_effective_progs with NULL,
    overwriting cgrp->bpf.effective[type] with NULL.
    
    Fix by removing the redundant cgrp-specific calls, matching the upstream
    pattern used in both a6010 and sdm710 reference kernels.

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -243,9 +243,6 @@ int __cgroup_bpf_attach(struct cgroup *cgrp, struct bpf_prog *prog,
 	cgrp->bpf.flags[type] = flags;
 
 	/* allocate and recompute effective prog arrays */
-	err = compute_effective_progs(cgrp, type, &cgrp->bpf.inactive);
-	if (err)
-		goto cleanup;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
@@ -255,8 +252,6 @@ int __cgroup_bpf_attach(struct cgroup *cgrp, struct bpf_prog *prog,
 	}
 
 	/* all allocations were successful. Activate all prog arrays */
-	activate_effective_progs(cgrp, type, cgrp->bpf.inactive);
-	cgrp->bpf.inactive = NULL;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
@@ -275,8 +270,6 @@ cleanup:
 	/* oom while computing effective. Free all computed effective arrays
 	 * since they were not activated
 	 */
-	bpf_prog_array_free(cgrp->bpf.inactive);
-	cgrp->bpf.inactive = NULL;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
@@ -348,9 +341,6 @@ int __cgroup_bpf_detach(struct cgroup *cgrp, struct bpf_prog *prog,
 	}
 
 	/* allocate and recompute effective prog arrays */
-	err = compute_effective_progs(cgrp, type, &cgrp->bpf.inactive);
-	if (err)
-		goto cleanup;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
@@ -360,8 +350,6 @@ int __cgroup_bpf_detach(struct cgroup *cgrp, struct bpf_prog *prog,
 	}
 
 	/* all allocations were successful. Activate all prog arrays */
-	activate_effective_progs(cgrp, type, cgrp->bpf.inactive);
-	cgrp->bpf.inactive = NULL;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
@@ -384,8 +372,6 @@ cleanup:
 	/* oom while computing effective. Free all computed effective arrays
 	 * since they were not activated
 	 */
-	bpf_prog_array_free(cgrp->bpf.inactive);
-	cgrp->bpf.inactive = NULL;
 	css_for_each_descendant_pre(css, &cgrp->self) {
 		struct cgroup *desc = container_of(css, struct cgroup, self);
 
