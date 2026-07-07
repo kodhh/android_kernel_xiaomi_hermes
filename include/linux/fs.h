5d0d96a5068c8539e88ecc64ebb27bbe4f497fda
root <root@runnervmkkn4f.1fzripawhdtutpjrtzqvefah5f.dx.internal.cloudapp.net>
2026-07-07 13:40:03 +0000

    bpf: fix seq_file const + simple_rename_flags
    - seq_file: add const struct file *file (Linus commit 34dbbcd)
    - seq_file: remove compat #ifdef CONFIG_USER_NS user_ns
    - file_inode: accept const struct file *f
    - bpf/inode: use simple_rename_flags instead of mismatched
      simple_rename for rename2 callback (5-arg vs 4-arg)
    - fuse/inode: remove BDI_CAP_STRICTLIMIT

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2323,7 +2323,7 @@ static inline bool execute_ok(struct inode *inode)
 	return (inode->i_mode & S_IXUGO) || S_ISDIR(inode->i_mode);
 }
 
-static inline struct inode *file_inode(struct file *f)
+static inline struct inode *file_inode(const struct file *f)
 {
 	return f->f_inode;
 }
